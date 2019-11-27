af253e7f46c4f2d87f759587b28e0389ccec4a21
artseven <yabkevich.artsiom.09@gmail.com>
2019-11-27 12:07:44 -0500

    ability to add quotes through the form

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
+    <app-new-quote @quoteAdded="addQuote"></app-new-quote>
     <app-quote-grid :quotes="quotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
 import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
 export default {
   data: () => {
     return {
@@ -14,7 +16,13 @@ export default {
     };
   },
   components: {
-    appQuoteGrid: QuoteGrid
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote
+  },
+  methods: {
+    addQuote(quote) {
+      this.quotes.push(quote);
+    }
   }
 };
 </script>
