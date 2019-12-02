6dd3973b9216f959b88a0e1914ee4396b530a171
artseven <yabkevich.artsiom.09@gmail.com>
2019-12-02 10:35:52 -0500

    added deleteQuote method

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="container">
     <app-new-quote @quoteAdded="addQuote"></app-new-quote>
-    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <app-quote-grid
+      :quotes="quotes"
+      @quoteDeleted="deleteQuote"
+    ></app-quote-grid>
     <div class="row">
       <div class="col-sm-12 text-center">
         <div class="alert alert-info">Info: Click on a Quote to delete it</div>
@@ -27,6 +30,9 @@ export default {
   methods: {
     addQuote(quote) {
       this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
     }
   }
 };
--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="row">
-    <app-quote v-for="quote in quotes" :key="quote.length">{{
-      quote
-    }}</app-quote>
+    <app-quote
+      v-for="(quote, index) in quotes"
+      :key="index"
+      @click.native="deleteQuote(index)"
+      >{{ quote }}</app-quote
+    >
   </div>
 </template>
 
@@ -12,6 +15,11 @@ export default {
   props: ["quotes"],
   components: {
     appQuote: Quote
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    }
   }
 };
 </script>
